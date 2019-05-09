09757a9a9dde7982714336977e7df54bfd7dd244
Arun <vv.arun@ymail.com>
2019-05-09 18:26:37 +0530

    Created Quadratic solver in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Button, Checkbox, Form, Container, Grid, Input, Header} from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function calc(){
+      let a = Number.parseInt(document.querySelector("#a").value);
+        let b = Number.parseInt(document.querySelector("#b").value);
+        let c = Number.parseInt(document.querySelector("#c").value);
+        let b2 = (b * b) - (4 * a * c);
+        let decider = Math.sqrt(b2).toString();
+        let left = (-b) / (2 * a);
+        let right = "";
+        let x1 = "";
+        let x2 = "";
+        if (decider === "NaN") {
+            right = (Math.sqrt(b2*-1)/(2 * a)).toString() + "i";
+            x1 = left.toString()+"+"+right.toString();
+            x2 = left.toString()+"-"+right.toString();
+        }
+        else{
+
+            right = decider/(2*a);
+            x1 = left+right;
+            x2 = left-right;
+        }
+        document.querySelector("#x1").value = x1;
+        document.querySelector("#x2").value = x2;
+
+}
+
+class App extends Component {
+
+    render() {
+        return (
+            <Container className="ui raised segment centered">
+                <Header as='h1' className="ui centered">Ax2+Bx+C=0</Header>
+                <Grid columns={3}>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Input id="a" className="ui  aligned center" label="A" placeholder='Enter A'/>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Input id="b" className="ui aligned center" label="B" placeholder='Enter B'/>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Input id="c" className="ui aligned center" label="C" placeholder='Enter C'/>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+                <Grid columns={2}>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Input id="x1" className="ui  aligned center" label="x1" readOnly={true}/>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Input id="x2" className="ui aligned center" label="x2" readOnly={true}/>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+                <Button id="submitbtn" onClick={calc} className="ui centered"> Calculate</Button>
+            </Container>
+        );
+    }
 }
 
 export default App;
---
+++ src/containers/header.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+import "../../node_modules/semantic-ui-css/semantic.min.css"
+
+class Header extends Component {
+
+    render(){
+        return (
+
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+
+export default Header;
